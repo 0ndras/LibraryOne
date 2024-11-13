67e3864584ace3013c89211cd3024b8001be2d9e
Andrei2101 <andreiandras529@yahoo.com>
2024-11-13 18:12:21 +0200

    Added initial project save and delete functionalities with uber jar

---
+++ src/main/java/service/BookService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.Book;
+
+import java.util.*;
+
+public interface BookService {
+    List<Book> findAll();
+    Book findById(Long id);
+
+    boolean save(Book book);
+
+    boolean delete (Book book);
+
+    int getAgeOfBook(Long id);
+}
